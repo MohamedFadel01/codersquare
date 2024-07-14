cd67620a15c222c5d03da2976bb52673907060dc
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-07-14 16:37:44 +0300

    Implement User model

---
+++ server/src/models/userModel.js
@@ -0,0 +1,40 @@
+import sequelize from "./dbConnection";
+import { DataTypes, Model, STRING } from "sequelize";
+
+class User extends Model {}
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+  }
+);
+
+export default User;
