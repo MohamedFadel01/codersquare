1ae7b1a3669d369a7b3bbd322f4cf4475b8fc6cf
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-08-13 22:46:17 +0300

    feat(likes): implement createLike controller for adding likes to posts

--- server/src/controllers/likeController.js
+++ server/src/controllers/likeController.js
@@ -1,8 +1,35 @@
+import Like from "../models/likeModel.js";
 import User from "../models/userModel.js";
 import Post from "../models/postModel.js";
 import errorMsgSender from "../utils/errorMsgSender.js";
 import validator from "validator";
+import log from 'fancy-log';
+import { ForeignKeyConstraintError, UniqueConstraintError } from "sequelize";
 
-export const createLike = async (req, res) => {};
+export const createLike = async (req, res) => {
+  const { postId } = req.body;
+  if (!postId) {
+    return errorMsgSender(res, 400, "required fields are missing");
+  }
+  if (!validator.isUUID(postId)) {
+    return errorMsgSender(res, 400, "invalid post id");
+  }
+
+  try {
+    await Like.create({ userId: res.locals.userId, postId });
+    res.sendStatus(201);
+  } catch (error) {
+    log.error(error);
+    if (error instanceof ForeignKeyConstraintError) {
+      return errorMsgSender(res, 404, "post not found");
+    }
+    if (error instanceof UniqueConstraintError) {
+      return errorMsgSender(res, 400, "like already exists");
+    }
+    res.sendStatus(500);
+  }
+};
+
 export const getPostLikes = async (req, res) => {};
+
 export const deleteLike = async (req, res) => {};
