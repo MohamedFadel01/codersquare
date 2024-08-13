39720710867fb100ce81b67cbe582d31254c0078
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-08-13 14:42:34 +0300

    feat(comments): implement createComment controller for adding comments

--- server/src/controllers/commentController.js
+++ server/src/controllers/commentController.js
@@ -5,8 +5,34 @@ import errorMsgSender from "../utils/errorMsgSender.js";
 import validator from "validator";
 import log from "fancy-log";
 import crypto from "crypto";
+import { ForeignKeyConstraintError } from "sequelize";
 
-export const createComment = async (req, res) => {};
+export const createComment = async (req, res) => {
+  const id = crypto.randomUUID();
+  const { postId, body } = req.body;
+  if (!postId || !body) {
+    return errorMsgSender(res, 400, "required fields mre missing");
+  }
+  if (!validator.isUUID(postId)) {
+    return errorMsgSender(res, 400, "invalid post id");
+  }
+
+  try {
+    await Comment.create({
+      id,
+      userId: res.locals.userId,
+      postId,
+      body,
+    });
+    res.status(200).json(id);
+  } catch (error) {
+    log.error(error);
+    if (error instanceof ForeignKeyConstraintError) {
+      return errorMsgSender(res, 404, "post not found");
+    }
+    res.sendStatus(500);
+  }
+};
 
 export const getPostComments = async (req, res) => {};
 
